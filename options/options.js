68077d43f0e3c1bee34f7aa851c198492bc508f8
Crowdbotics <team@crowdbotics.com>
2021-09-17 19:00:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "bitballer_30649Identifier",
+  url: "https://bitballer_30649Identifier.botics.co",
+  api: "https://bitballer_30649Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
